ee7d521531f8900b83790c0820d139e1895d760b
Paul Pelletier <paul.alain.pelletier@gmail.com>
2024-03-09 15:30:22 +0100

    Major changes

--- MarketDataDumper.py
+++ MarketDataDumper.py
@@ -1,14 +1,53 @@
-import yfinance as yf
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 import requests
-
-AlphaVantageAPIKey = "HF8YN14QRF5L1GGQ"
+import csv
+from io import StringIO
+import json
+import pandas as pd
 
 class TickerDataBase:
     def __init__(self) -> None:
+        self.apiKey = "EKZRGALCGSWPSD61"
         self.StockMarket = None
         self.CommoditiesMarket = None
         self.CryptoMarket = None
-        self.EconomicIndicators = None
+        self.EconomicIndicators = {
+                                    "Real GDP": {"function": "REAL_GDP", "interval": "quarterly"},
+                                    "Real GDP per Capita": {"function": "GDP_PER_CAPITA"},
+                                    "Treasury Yield": {"function": "TREASURY_YIELD", "interval": "daily", "maturity": ["3month", "2year", "5year", "7year", "10year", "30year"]},
+                                    "Federal Funds Rate": {"function": "FEDERAL_FUNDS_RATE", "interval": "daily"},
+                                    "CPI": {"function": "CPI", "interval": "monthly"},
+                                    "Inflation": {"function": "INFLATION", "interval": "monthly"},
+                                    "Retail Sales": {"function": "RETAIL_SALES", "interval": "monthly"},
+                                    "Durable Goods Orders": {"function": "DURABLES", "interval": "monthly"},
+                                    "Unemployment Rate": {"function": "UNEMPLOYMENT", "interval": "monthly"},
+                                    "Nonfarm Payroll": {"function": "NONFARM_PAYROLL", "interval": "monthly"}}
+    
+    def RequestAndDumpData(self : object, EconomicIndicator : str, url : str)->str:
+        response = requests.get(url)
+        data = json.loads(response.text)
+        with open(f"{EconomicIndicator}.json", 'w') as json_file:
+            json.dump(data, json_file, indent=4)
+        dataFrame = self.TransformJsonToPandasDataFrame(EconomicIndicator)
+        dataFrame.to_csv(f"{EconomicIndicator}.csv", sep = ";", index = False, header = True)
+        print("Done")
+        return "Done"
+    
+    def TransformJsonToPandasDataFrame(self : object, EconomicIndicator : str) -> pd.DataFrame:
+        with open(f'{EconomicIndicator}.json') as file:
+            json_data = json.load(file)
+        dataFrame = pd.DataFrame(json_data["data"])
+        dataFrame['date'] = pd.to_datetime(dataFrame["date"])
+        dataFrame['value'] = pd.to_numeric(dataFrame['value'], errors = "coerce")
+        return dataFrame
+    
+    def GetEconomicIndicatorsData(self) -> str:
+        for key in self.EconomicIndicators.keys():
+            economicIndicator = self.EconomicIndicators[key]
+            if key == "Treasury Yield":
+                for maturity in self.self.EconomicIndicators[key]["maturity"]:
+                    url = f"https://www.alphavantage.co/query?function={key}&interval={"daily"}&maturity={maturity}&apikey=" + self.apiKey
+            else:
+                url = f"https://www.alphavantage.co/query?function={key}&interval={"daily"}&&apikey=" + self.apiKey
+            self.RequestAndDumpData(economicIndicator, url)
+
+            
--- test.py
+++ test.py
@@ -1,18 +1,31 @@
 import requests
 import csv
 from io import StringIO
+from MarketDataDumper import TickerDataBase
 
-api_key = "HF8YN14QRF5L1GGQ"
+api_key = "EKZRGALCGSWPSD61"
+url = "https://www.alphavantage.co/query?function=TREASURY_YIELD&interval=daily&maturity=3M&apikey=" + api_key
+#dumper = TickerDataBase().RequestAndDumpData("Real GDP", url)
+# def get_tickers(api_key):
+#     economicData = TickerDataBase().EconomicIndicators["Treasury Yield"]
+#     url = "https://www.alphavantage.co/query?function=TREASURY_YIELD&interval=daily&maturity=3M&apikey=" + api_key
+#     response = requests.get(url)
+#     csvfile = StringIO(response.text)
+#     reader = csv.reader(csvfile)
+#     tickers = [row for row in reader]  # Adjust based on the CSV structure
+#     with open('tickers.csv', 'w') as file:
+#         file.write(response.text)
+#     return tickers
 
-def get_tickers(api_key):
-    url = "https://www.alphavantage.co/query?function=UNEMPLOYMENT&interval=weekly&apikey=" + api_key
-    response = requests.get(url)
-    csvfile = StringIO(response.text)
-    reader = csv.reader(csvfile)
-    tickers = [row for row in reader]  # Adjust based on the CSV structure
-    with open('tickers.csv', 'w') as file:
-        file.write(response.text)
-    return tickers
-
-tickers = get_tickers(api_key)
-print(tickers)
+# tickers = get_tickers(api_key)
+# print(tickers)
+import pandas as pd
+import json
+with open('Real GDP.json') as f:
+    json_data = json.load(f)
+df = pd.DataFrame(json_data["data"])
+df['date'] = pd.to_datetime(df["date"])
+df['value'] = pd.to_numeric(df['value'], errors = "coerce")
+dataFrame = TickerDataBase().TransformJsonToPandasDataFrame("Real GDP")
+dataFrame.to_csv("Real GDP.csv", sep = ";", index = False, header = True)
+print(df['value'][1]+df['value'][2])
