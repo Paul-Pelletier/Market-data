097648b0f11e44623e37ddec49f5f27147aa0d21
Paul Pelletier <paul.alain.pelletier@gmail.com>
2024-03-09 16:53:38 +0100

    PyLint thing

--- test.py
+++ test.py
@@ -1,12 +1,6 @@
-import requests
-import csv
-from io import StringIO
-import pandas as pd
-import json
 from MarketDataDumper import MarketDataRequestAndDump
 
 api_key = "EKZRGALCGSWPSD61"
 "EKZRGALCGSWPSD61"
 
-a = MarketDataRequestAndDump(api_key)
-b = a.GetEconomicIndicatorsData()
+a = MarketDataRequestAndDump(api_key).GetEconomicIndicatorsData()
